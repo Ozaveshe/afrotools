d7656539d116dd0da1df431d6456905abe4b8aec
Oza <momohozaveshe@gmail.com>
2026-09-09 20:56:03 +0500

    Synchronize generated operator route metadata and cache stamp

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  *
  * CACHE_VERSION is stamped by `npm run build`; changing it purges old caches.
  */
-const CACHE_VERSION = '0ceba831';
+const CACHE_VERSION = '92429a57';
 const CACHE_NAME = `afrotools-v${CACHE_VERSION}`;
 const OFFLINE_URL = '/offline.html';
 
