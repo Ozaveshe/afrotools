3c47e9ec655e9ea3ab626b218e0e0c544e890239
Oza <momohozaveshe@gmail.com>
2026-09-12 14:13:16 +0500

    Integrate 190 verified Biology questions with compatible reviewed diagrams

---
+++ ops/jamb/verification/check-biology-1987-006.cjs
@@ -0,0 +1,2 @@
+'use strict';
+require('./check-integrated-subject-batch.cjs')("biology-1987-publishable-006.json");
---
+++ ops/jamb/verification/check-biology-1989-008.cjs
@@ -0,0 +1,2 @@
+'use strict';
+require('./check-integrated-subject-batch.cjs')("biology-1989-publishable-008.json");
