1115b2200248ef799643f2d40d2de17f06474ce5
Oza <momohozaveshe@gmail.com>
2026-09-12 08:43:52 +0500

    Regenerate release artifacts for 246 reviewed JAMB questions

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  *
  * CACHE_VERSION is stamped by `npm run build`; changing it purges old caches.
  */
-const CACHE_VERSION = '9b21c240';
+const CACHE_VERSION = 'e4a78b89';
 const CACHE_NAME = `afrotools-v${CACHE_VERSION}`;
 const OFFLINE_URL = '/offline.html';
 
