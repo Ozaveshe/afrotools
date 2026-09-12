8fea86c2f859758d0b5ff1bb54ccc39a96e12ed5
Oza <momohozaveshe@gmail.com>
2026-09-12 12:05:02 +0500

    Integrate 325 checked English and Chemistry questions

---
+++ ops/jamb/verification/check-chemistry-2001-001.cjs
@@ -0,0 +1,2 @@
+'use strict';
+require('./check-integrated-subject-batch.cjs')("chemistry-2001-publishable-001.json");
---
+++ ops/jamb/verification/check-english-2003-001.cjs
@@ -0,0 +1,2 @@
+'use strict';
+require('./check-integrated-subject-batch.cjs')("english-2003-publishable-001.json");
