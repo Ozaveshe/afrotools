23d738c2baa0c28b48f95ba88abd659589c4eee2
Oza <momohozaveshe@gmail.com>
2026-09-16 16:31:27 +0500

    fix: preserve wage CSV precision and localize Swahili exports

--- scripts/build-sw-salary-tool-pages.js
+++ scripts/build-sw-salary-tool-pages.js
@@ -476,7 +476,7 @@ function buildSwObservation(c) {
   ]);
 
   html = replaceRegex(html, [
-    [/var opts = '<option value="">[^<]*National \/ all states[^<]*<\/option>';/, "var opts = '<option value=\"\">a-" Taifa / majimbo yote a-"</option>';"],
+    [/var opts = '<option value="">[^<]*National \/ all states[^<]*<\/option>';/, "var opts = '<option value=\"\">Taifa / majimbo yote</option>';"],
     [/dHTML \+= '<div class="mw-info-row"><span class="mw-info-label">Effective Date<\/span><span class="mw-info-val">' \+ \(sr\.effectiveDate \|\| c\.effectiveDate\) \+ '<\/span><\/div>';/, "dHTML += '<div class=\"mw-info-row\"><span class=\"mw-info-label\">Tarehe ya kuanza</span><span class=\"mw-info-val\">' + (sr.effectiveDate || c.effectiveDate) + '</span></div>';"],
     [/canvas\.setAttribute\('aria-label', c\.name \+ ' minimum wage history ' \+ c\.history\[c\.history\.length-1\]\.year \+ '[^']*' \+ c\.history\[0\]\.year\);/, "canvas.setAttribute('aria-label', 'Historia ya kima cha chini cha mshahara ya ' + c.name + ' ' + c.history[c.history.length-1].year + '-' + c.history[0].year);"],
     [/titleEl\.(?:innerHTML|textContent) = '[^']*No statutory minimum wage in ' \+ result\.flag \+ ' ' \+ result\.country;/, "titleEl.innerHTML = '- Hakuna kima cha chini cha mshahara cha kitaifa katika ' + result.flag + ' ' + swCountryName(_currentCode, result.country);"],
@@ -492,6 +492,7 @@ function buildSwObservation(c) {
     [/html \+= '<div class="mw-cc-row"><span class="mw-cc-label">Effective<\/span><span class="mw-cc-val">' \+ (?:c|country)\.effectiveDate \+ '<\/span><\/div>';/g, "html += '<div class=\"mw-cc-row\"><span class=\"mw-cc-label\">Imeanza</span><span class=\"mw-cc-val\">' + c.effectiveDate + '</span></div>';"],
   ]);
 
+  html = html.replace('var csv = E.exportCSV();', "var csv = E.exportCSV('sw');");
   write('sw/zana/kikokotoo-kima-cha-chini-cha-mshahara/index.html', html);
 }
 
