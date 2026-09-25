daaa7365f16a845ae8f6b4e4dc1b093dfaf7e213
Oza <momohozaveshe@gmail.com>
2026-09-26 00:48:17 +0500

    chore(education): refresh deployed routes for verified exam guides

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  *
  * CACHE_VERSION is stamped by `npm run build`; changing it purges old caches.
  */
-const CACHE_VERSION = 'f2ab1266';
+const CACHE_VERSION = 'ce1c7145';
 const CACHE_NAME = `afrotools-v${CACHE_VERSION}`;
 const OFFLINE_URL = '/offline.html';
 
